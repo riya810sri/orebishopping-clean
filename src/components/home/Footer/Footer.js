3191831f00a93decd9ac0157a4887afab112dfb7
Riya Srivastava <156165467+riya810sri@users.noreply.github.com>
2025-07-10 00:04:00 +0530

    Update Footer.js

--- src/components/home/Footer/Footer.js
+++ src/components/home/Footer/Footer.js
@@ -39,7 +39,7 @@ const Footer = () => {
             </p>
             <ul className="flex items-center gap-2">
               <a
-                href="https://www.youtube.com/@reactjsBD"
+                href="https://www.youtube.com/@starshadow262"
                 target="_blank"
                 rel="noreferrer"
               >
@@ -48,7 +48,7 @@ const Footer = () => {
                 </li>
               </a>
               <a
-                href="https://github.com/noorjsdivs"
+                href="https://github.com/riya810sri"
                 target="_blank"
                 rel="noreferrer"
               >
@@ -57,7 +57,7 @@ const Footer = () => {
                 </li>
               </a>
               <a
-                href="https://www.facebook.com/Noorlalu143/"
+                href="https://www.facebook.com/profile.php?id=61558848598441"
                 target="_blank"
                 rel="noreferrer"
               >
@@ -66,7 +66,7 @@ const Footer = () => {
                 </li>
               </a>
               <a
-                href="https://www.linkedin.com/in/noor-mohammad-ab2245193/"
+                href="https://www.linkedin.com/in/riya-srivastava-5649b72a6/"
                 target="_blank"
                 rel="noreferrer"
               >
